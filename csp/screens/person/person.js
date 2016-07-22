3fa55f48706466e9245154aa43d4c9aee1354e39
Klomanton <Klomanton@gmail.com>
2016-07-22 17:22:14 +0700

    add person.js and person.html working tree template

---
+++ csp/screens/person/person.js
@@ -0,0 +1,6 @@
+ 'use strict';
+
+ angular.module('app')
+ .controller('PersonController', ['$scope', function($scope) {
+  		console.log("Пловобок");
+  }]);
--- csp/tree/tree.js
+++ csp/tree/tree.js
@@ -1,9 +1,17 @@
 var treeModule = angular.module('ui.tree',[]);
 
-treeModule.directive('tree',function($scope,$http){
-	$scope.person=[];
-	$scope.loadPerson=function(){
-		$http({method:'GET',url:getPersons})
+treeModule.directive('tree', function(){
+	return{
+		replace: true,
+		restrict: 'E',
+		templateUrl: 'tree/tree.html',
 
-	};
-});
+		scope:{
+
+		},
+		controller: ['$scope',function($scope){
+				console.log('tree initiated epta!11');
+		}]
+	}	
+	}
+);
