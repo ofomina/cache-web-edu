c3800d80e4ca404a705fb7ae0b455aae3121294e
Klomanton <Klomanton@gmail.com>
2016-07-25 14:12:41 +0700

    add edit screen for person

---
+++ csp/screens/edit/edit.js
@@ -0,0 +1,8 @@
+ 'use strict';
+
+ angular.module('app')
+ .controller('PersonEdit', ['$scope','PersonSrvc', function($scope,PersonSrvc) {
+  		console.log("plovobok");
+  	//	$scope.Persons = PersonSrvc.GetAll; 
+
+  }]);
