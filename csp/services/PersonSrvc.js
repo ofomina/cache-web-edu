c8ff26f92f444611a32201de5307119a35653a15
Klomanton <Klomanton@gmail.com>
2016-07-25 17:44:37 +0700

    add submit form to edit/edit.html and edit/edit.js add save to PersonSrvc.js

--- csp/services/PersonSrvc.js
+++ csp/services/PersonSrvc.js
@@ -4,4 +4,8 @@ servicesModule.factory('PersonSrvc', ["RESTSrvc", function(RESTSrvc) {
 	return {
 		getAll: function(){
             return RESTSrvc.getPromise({method: 'GET' , url: 'broker/GetPersons'});
-		}}}]);
+		},
+		save: function(object){
+			console.log(object);
+		}
+	}}]);
--- csp/services/StatusSrvc.js
+++ csp/services/StatusSrvc.js
@@ -4,4 +4,6 @@ servicesModule.factory('StatusSrvc', ["RESTSrvc", function(RESTSrvc) {
 	return {
 		getAll: function(){
             return RESTSrvc.getPromise({method: 'GET' , url: 'broker/GetStatuses'});
-		}}}]);
+		}
+	}
+}]);
