a4bebbc89095878bcfb944927f8af2b7e51643f6
Klomanton <Klomanton@gmail.com>
2016-07-26 12:52:26 +0700

    getAll to POST

--- csp/services/PersonSrvc.js
+++ csp/services/PersonSrvc.js
@@ -2,8 +2,8 @@
 
 servicesModule.factory('PersonSrvc', ["RESTSrvc", function(RESTSrvc) {	
 	return {
-		getAll: function(){
-            return RESTSrvc.getPromise({method: 'GET' , url: 'broker/GetPersons'});
+		getAll: function(parentId,columns){
+            return RESTSrvc.getPromise({method: 'POST' , url: 'broker/GetPersons',data:{parentId:parentId,columns:columns}});
 		},
 		save: function(object){
 			return RESTSrvc.getPromise({method: 'POST',url:'broker/person/save', data:object});
