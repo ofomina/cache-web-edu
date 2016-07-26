d8a050dad493b286ba75e70f067ee4057cb42464
Klomanton <Klomanton@gmail.com>
2016-07-26 15:43:42 +0700

    add save by id

--- csp/screens/edit/edit.js
+++ csp/screens/edit/edit.js
@@ -1,21 +1,37 @@
  'use strict';
 
  angular.module('app')
- .controller('PersonEdit', ['$scope','StatusSrvc','PersonSrvc', function($scope,StatusSrvc,PersonSrvc) {
+ .controller('PersonEdit', ['$scope','$state','StatusSrvc','PersonSrvc', function($scope,$state,StatusSrvc,PersonSrvc) {
   		console.log("plovobok");
   		$scope.object = {};
+  		$scope.id = $state.params.id;
+  		
   		$scope.save = function(){
   			console.log('goTo PersonSrvc');
   			PersonSrvc.save($scope.object);
   		};
+
+  		if ($scope.id) {
+  			PersonSrvc.get($scope.id).then(
+  				function(data,status,headers,config){
+  					console.log("good person");
+  					$scope.object = data.data;
+  					console.log($scope.object);
+  				},
+  				function(data,status,headers,config){
+  					console.log("bad person");
+  				}
+  			);
+		}
+		
   		StatusSrvc.getAll().then(
   			function(data,status,headers,config) {
   				$scope.statuses = data.data.list;
-  				console.log(data);
-  				console.log("good");
+  				//console.log(data);
+  				console.log("good status");
   			},
   			function(data,status,headers,config) {
-  				console.log("bad");
+  				console.log("bad status");
   			}
   		); 
 
--- csp/services/PersonSrvc.js
+++ csp/services/PersonSrvc.js
@@ -5,7 +5,11 @@ servicesModule.factory('PersonSrvc', ["RESTSrvc", function(RESTSrvc) {
 		getAll: function(parentId,columns){
             return RESTSrvc.getPromise({method: 'POST' , url: 'broker/GetPersons',data:{parentId:parentId,columns:columns}});
 		},
+		get: function(personId){
+			return RESTSrvc.getPromise({method:'GET',url:'broker/person/'+personId});
+		},
 		save: function(object){
+			console.log(object);
 			return RESTSrvc.getPromise({method: 'POST',url:'broker/person/save', data:object});;
 		},
 		remove: function(id){
